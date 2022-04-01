560e727e878b59d28b92e63524271dc980875d01
Jerish Bovas <jerishbradlyb@gmail.com>
2022-04-01 13:28:38 -0400

    routing redefined

--- public/js/index.js
+++ public/js/index.js
@@ -22,11 +22,10 @@ link.href = 'https://chat-bot-web-app.azurewebsites.net/startup.css';
 head.appendChild(link);
 
 head.innerHTML += '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"></link>';
-head.innerHTML += '<script src="https://kit.fontawesome.com/c11ddac06e.js" crossorigin="anonymous"></script>';
 
 
 let chatButton = document.createElement('button')
-chatButton.innerHTML = '<i class="fa-solid fa-robot"></i>';
+chatButton.innerHTML = 'Ask Bot';
 chatButton.setAttribute('id', 'chatButton');
 chatButton.position = 'absolute';
 chatButton.onclick = openChatbot
