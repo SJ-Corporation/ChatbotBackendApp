1dd025a0a97f013ebde49b2a0722d8bad85f7621
Jerish Bovas <jerishbradlyb@gmail.com>
2022-04-01 11:59:03 -0400

    env port added

--- www/js/index.js
+++ www/js/index.js
@@ -18,7 +18,7 @@ function closeChatbot(){
 let link = newE('link')
 link.rel = 'stylesheet';
 link.type = 'text/css';
-link.href = 'https://chat-bot-web-app.azurewebsites.net/style.css';
+link.href = '/www/css/style.css';
 head.appendChild(link);
 
 head.innerHTML += '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"></link>';
